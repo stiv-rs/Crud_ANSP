e71d44aed1305bbcc302230d88afe54c47970714
Stiv <strodriguezna@gmail.com>
2022-03-23 19:02:48 -0500

    Se agrega la funcionalidad de DELETE

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,3 +1,4 @@
+import  swal  from 'sweetalert2';
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import {ClienteService} from './cliente.service';
@@ -14,8 +15,42 @@ export class ClientesComponent implements OnInit {
 
   ngOnInit(): void {
     this.clienteService.getClientes().subscribe(
-      clientes => this.clientes = clientes 
+      clientes => this.clientes = clientes
     );
+  }
+
+  delete(cliente:Cliente):void{
+    const swalWithBootstrapButtons = swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false
+    })
+
+    swalWithBootstrapButtons.fire({
+      title: 'Estas seguro?',
+      text: `Estas a punto de eliminar a ${cliente.nombre} ${cliente.apellido}`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Si, borrar!',
+      cancelButtonText: 'No, cancelar!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        this.clienteService.delete(cliente.id).subscribe(
+          response =>{
+            this.clientes = this.clientes.filter(cli => cli !== cliente)
+            swalWithBootstrapButtons.fire(
+              'Cliente Eliminado!',
+              `Has eliminado a ${cliente.nombre}.`,
+              'success'
+            )
+          }
+        )
+      }
+    })
   }
 
 }
