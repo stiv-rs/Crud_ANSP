6b403a777394d0c1185d60217cd0e13267be8e22
Stiv <strodriguezna@gmail.com>
2022-05-31 17:17:34 -0500

    Añadiendo el METODO LOGIN en el componente y nueva CLASE USUARIO

--- src/app/usuarios/login.component.ts
+++ src/app/usuarios/login.component.ts
@@ -1,4 +1,7 @@
+import { Usuario } from './usuario';
 import { Component, OnInit } from '@angular/core';
+import swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-login',
@@ -7,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   titulo: string = 'Porfavor ingrese sus credenciales';
+  usuario:Usuario;
 
-  constructor() { }
+  constructor() {
+    this.usuario = new Usuario();
+  }
 
   ngOnInit(): void {
+  }
+
+  login():void{
+    console.log(this.usuario);
+    if (this.usuario.username == null || this.usuario.password == null) {
+      swal.fire('Error Login', 'Username o password vacias!', 'error');
+      return;
+    }
   }
 
 }
