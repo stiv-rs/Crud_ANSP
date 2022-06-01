52dad0b068874c9d01eb916700b6f334911b6279
Stiv <strodriguezna@gmail.com>
2022-06-01 15:26:25 -0500

    Creación de la clase AUTHSERVICE para la autorizacion y en ella el metodo login

---
+++ src/app/usuarios/auth.service.ts
@@ -0,0 +1,28 @@
+import { Usuario } from './usuario';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { URLSearchParams } from 'url';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http:HttpClient) { }
+
+  login(usuario:Usuario):Observable<any>{
+    const urlEndpoint = 'http://localhost:8080/oauth/token';
+    const credenciales= btoa('angularapp'+':'+'12345');
+    const httpHeaders = new HttpHeaders({'Content-Type':'application/x-www-form-urlencoded', 'Authorization': 'Basic' + credenciales});
+    let params = new URLSearchParams();
+    params.set('grant_type', 'password');
+    params.set('username', usuario.username);
+    params.set('username', usuario.password);
+
+    console.log(params.toString());
+
+    return this.http.post(urlEndpoint, params.toString(),{headers:httpHeaders});
+  }
+
+}
