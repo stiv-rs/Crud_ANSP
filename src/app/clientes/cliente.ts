57ab03e67d532e7486ff202792b7a20ed363af02
Stiv <strodriguezna@gmail.com>
2022-06-28 09:49:12 -0500

    Agregando las calses ENTITY FACTURA, PRODUCTO, ITEM-FACTURA

--- src/app/clientes/cliente.ts
+++ src/app/clientes/cliente.ts
@@ -1,3 +1,4 @@
+import { Factura } from '../facturas/models/factura';
 import { Region } from './region';
 
 export class Cliente {
@@ -8,4 +9,5 @@ export class Cliente {
   email: string;
   foto: string;
   region: Region;
+  facturas: Array<Factura> = [];
 }
