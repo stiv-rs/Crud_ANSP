c03c72188e07404637a26ee9ff49412c86efd43e
Stiv <strodriguezna@gmail.com>
2022-10-06 20:27:26 -0500

    ELIMINA la linea de ITEM de factura

--- src/app/facturas/facturas.component.ts
+++ src/app/facturas/facturas.component.ts
@@ -70,6 +70,11 @@ export class FacturasComponent implements OnInit {
 
   actualizarCantidad(id:number, event:any):void{
     let cantidad:number = event.target.value as number;
+
+    if (cantidad == 0) {
+      return this.eliminarItemFactura(id);
+    }
+
     this.factura.items = this.factura.items.map((item:ItemFactura) => {
       if (id === item.producto.id) {
         item.cantidad = cantidad;
@@ -95,6 +100,10 @@ export class FacturasComponent implements OnInit {
       }
       return item;
     });
+  }
+
+  eliminarItemFactura(id:number):void{
+    this.factura.items = this.factura.items.filter((item: ItemFactura) => id != item.producto.id);
   }
 
 }
