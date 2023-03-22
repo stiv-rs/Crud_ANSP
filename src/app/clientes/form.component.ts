c921a6cb0ba7a52ace7ee885d0c4d8786ade0c57
Stiv <strodriguezna@gmail.com>
2023-03-22 18:29:21 -0500

    Resolviendo un problema de RECURSIVIDAD

--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -50,6 +50,8 @@ export class FormComponent implements OnInit {
   }
 
   update():void{
+    console.log(this.cliente)
+    this.cliente.facturas = null;
     this.clienteService.update(this.cliente).subscribe(
       json => {
         this.router.navigate(['/clientes'])
